18acfb8339bd4a612b1f9bb1b6907a84fc093ef3
Jeremy Prater <silverevo03@gmail.com>
2019-04-10 12:11:48 -0700

    - Add capnproto deps

--- install_deps.sh
+++ install_deps.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yay -S libyaml ffmpeg-compat-57 libavresample python libsamplerate taglib chromaprint-fftw python-six vamp-plugin-sdk intel-tbb zeromq
+yay -S libyaml ffmpeg-compat-57 libavresample python libsamplerate taglib chromaprint-fftw python-six vamp-plugin-sdk intel-tbb zeromq capnproto
 
 # cd cpp-netlib
 # makepkg -si
