19a9eae407ba3779d78d279cc0f637e5415c4284
Jeremy Prater <silverevo03@gmail.com>
2019-07-22 00:14:02 -0700

    - Start working on lerp-color-theme... because linear interlop is the best

--- clients/resources/shaders/grid-1_ps.glsl
+++ clients/resources/shaders/grid-1_ps.glsl
@@ -14,8 +14,13 @@ out vec4 fragmentColor;
 void main() {
   vec4 outColor = vec4(0., 0., 0., 0.);
 
-  float lineRate = .15;
-  float widthScale = 10;
+  // float lineRate = .25; // V1 - Max
+  float lineRate = .1; // V2 - Avg
+  // float lineRate = 1.2; // V3 - Min
+
+  float widthScale = 10; // V1 - Max
+  // float widthScale = 10; // V2 - Avg
+  // float widthScale = 30; // V3 - Min
 
   float newX = interpolatedTextureCoordinates.x + theta;
   float newY = interpolatedTextureCoordinates.y;
@@ -25,8 +30,24 @@ void main() {
   float ySlice =
       pow(abs(fract((newY * numSlices) / widthScale) - .5), lineRate);
 
-  outColor = max(mix(vec4(accent1Color, 1.), outColor, ySlice),
-                 mix(vec4(accent2Color, 1.), outColor, xSlice));
+  // V1 - Max
+  // outColor = max(mix(vec4(accent1Color, 1.), outColor, ySlice),
+  //                mix(vec4(accent2Color, 1.), outColor, xSlice));
+
+  // V2 - Avg
+  outColor = ((mix(vec4(accent1Color, 1.), outColor, ySlice) +
+               mix(vec4(accent2Color, 1.), outColor, xSlice)) /
+              2);
+
+  // V3 - Min
+  // outColor = min(mix(vec4(accent1Color, 1.), outColor, 1 - ySlice),
+  //                mix(vec4(accent2Color, 1.), outColor, 1 - xSlice));
+
+  // Negative space
+  if (outColor.a < .25) { // V1 - Max
+  // if (outColor.a < .17) { // V3 - Min
+    discard;
+  }
 
   fragmentColor = outColor;
 }
